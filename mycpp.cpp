11f5b93e43e6b4255f9a22b2f90375053a07bbad
Srikrshna <srikrshna.01.p@gmail.com>
2023-02-14 21:49:36 +0530

    mucpp.cpp pushed

--- mycpp.cpp
+++ mycpp.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "This is being done using Jenkinsfile\n";
   cout << "I have successfully built and run\n";
   cout << "Name:Srikrshna P\tSRN:PES1UG20CS692\n";
-  cout << "This is the change made after adding the Web Hook\n";
   return 0;
 }
